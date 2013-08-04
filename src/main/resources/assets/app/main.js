e5fcdfd33ae50a2ac06c96849024ae221a7f132b
Adam Batkin <adam@batkin.net>
2013-08-03 22:52:05 -0400

    add main page

--- src/main/resources/assets/app/main.js
+++ src/main/resources/assets/app/main.js
@@ -1,7 +1,20 @@
 var app = angular.module('gitdown', []);
 
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'pages/main.html',
+        controller: 'MainPageController'
+    });
+}]);
+
 app.controller('GitDownController', ['$scope', function($scope) {
     $scope.page = {
         title: 'Welcome to GitDown'
     }
 }]);
+
+app.controller('MainPageController', ['$scope', function($scope) {
+    $scope.page.title = 'Main Page';
+}]);
+
+
